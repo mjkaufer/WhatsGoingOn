8e68ff5ed2c8d2e098acb6cf2c65935cf0fd4eb9
Matthew Kaufer <matthew@kaufer.org>
2014-10-08 17:04:15 -0400

    Phone number in credentials

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 var data = {};
 data = JSON.parse(Assets.getText("credentials.json"));
+Acc_SID = data.sid;
+Auth_token = data.authToken;
+var phoneNumber = data.from;
 
 twilioClient = Twilio(Acc_SID, Auth_token);
 
@@ -7,7 +10,13 @@ twilioClient = Twilio(Acc_SID, Auth_token);
 
 Meteor.methods({
 	'sendCall':function(number){
-
-		var auth = 
+	client.calls.create({
+		url: "call.xml",
+		to: "+14155551212",
+		from: "+14158675309"
+	}, function(err, call) {
+		process.stdout.write(call.sid);
+	});
+		
 	}
 })
